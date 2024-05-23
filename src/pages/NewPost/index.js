7e22d63195f1fef8b83ba80b9f74a76418f67492
CARALH0 <srcaralhocaralhus@gmail.com>
2024-05-23 01:57:58 -0300

    final

--- src/pages/NewPost/index.js
+++ src/pages/NewPost/index.js
@@ -5,73 +5,70 @@ import storage from '@react-native-firebase/storage';
 import firestore from '@react-native-firebase/firestore';
 import { AuthContext } from '../../contexts/auth';
 
-import { View, Text } from 'react-native';
 import { Container, Input, Button, ButtonText } from './styles';
 
 export default function NewPost() {
   const navigation = useNavigation();
   const [post, setPost] = useState('');
-  const { user} = useContext(AuthContext);
-  
-  useLayoutEffect(() =>{
-    const options = navigation.setOptions({
+  const { user } = useContext(AuthContext);
+
+  useLayoutEffect(() => {
+    navigation.setOptions({
       headerRight: () => (
-        <Button onPress={() =>  handlePost() }>
+        <Button onPress={() => handlePost()}>
           <ButtonText>Compartilhar</ButtonText>
         </Button>
-      )
-    })
+      ),
+    });
   }, [navigation, post]);
 
-  async function handlePost(){
-    if(post === ''){
-      console.log('Seu post contem conteudo invalido.')
+  async function handlePost() {
+    if (post === '') {
+      console.log('Seu post contém conteúdo inválido.');
       return;
     }
-    
+
     let avatarUrl = null;
-    try{
-      let response = await storage().ref('users').child(user?.uid).getDownloadURL();
+    try {
+      // Recupera a URL da imagem de perfil do Firebase Storage
+      const response = await storage().ref(`profileImages/${user?.uid}`).getDownloadURL();
       avatarUrl = response;
-
-
-
-    }catch(err){
+    } catch (err) {
       avatarUrl = null;
     }
 
+    // Adiciona o novo post no Firestore
     await firestore().collection('posts')
-    .add({
-      created: new Date(),
-      content: post,
-      autor: user.nome,
-      likes: 0,
-      avatarUrl,
-      userId: user.uid,
-    })
-    .then(() =>{
-      setPost('');
-      console.log('POST CRIADO COM SUCESSO');
-    })
-    .catch((error) =>{
-      console.log(error);
-    })
+      .add({
+        created: new Date(),
+        content: post,
+        autor: user.nome,
+        likes: 0,
+        avatarUrl,
+        userId: user.uid,
+      })
+      .then(() => {
+        setPost('');
+        console.log('POST CRIADO COM SUCESSO');
+      })
+      .catch((error) => {
+        console.log(error);
+      });
 
     navigation.goBack();
-    
   }
 
- return (
-   <Container>
-    <Input
-    placeholder="O que você está pensando"
-    placeholderTextColor="#DDD"
-    multiline={true}
-    maxLength={300}
-    value={post}
-    onChangeText={ (text) => setPost(text) }
-    autoCorrect={false}
-    />
-   </Container>
+  return (
+    <Container>
+      <Input
+        placeholder="O que você está pensando"
+        placeholderTextColor="#DDD"
+        multiline={true}
+        maxLength={300}
+        value={post}
+        onChangeText={(text) => setPost(text)}
+        autoCorrect={false}
+      />
+    </Container>
   );
 }
--- src/pages/Profile/index.js
+++ src/pages/Profile/index.js
@@ -1,58 +1,99 @@
-import React, { useContext, useState } from 'react';
-import { View, Text, Button, StyleSheet, TouchableOpacity, Image } from 'react-native';
+import React, { useContext, useState, useEffect } from 'react';
+import { View, Text, TouchableOpacity, Image, StyleSheet } from 'react-native';
 import ImageCropPicker from 'react-native-image-crop-picker';
 import { AuthContext } from '../../contexts/auth';
 import Feather from 'react-native-vector-icons/Feather';
-
-
+import { firebase } from '@react-native-firebase/storage';
+import firestore from '@react-native-firebase/firestore';
 
 export default function Profile() {
   const { signOut, user } = useContext(AuthContext);
-  const [profilePic, setProfilePic] = useState(null); // Armazena a foto de perfil
+  const [profilePic, setProfilePic] = useState(null);
 
-  const handleChooseImage = () => {
-    ImageCropPicker.openPicker({
-      width: 300,
-      height: 300,
-      cropping: true
-    }).then(image => {
-      // Atualiza o estado com a imagem selecionada
-      setProfilePic(image.path);
-      // Faz upload da imagem para o Firebase aqui
-    }).catch(error => {
-      console.log('Ocorreu um erro ao selecionar a imagem:', error);
+  useEffect(() => {
+    // Busca a imagem de perfil do Firestore ao carregar o componente
+    const userRef = firestore().collection('users').doc(user.uid);
+    userRef.get().then((docSnapshot) => {
+      if (docSnapshot.exists) {
+        const data = docSnapshot.data();
+        if (data.profileImageUrl) {
+          setProfilePic(data.profileImageUrl);
+        }
+      }
+    }).catch((error) => {
+      console.error('Erro ao buscar a imagem de perfil:', error);
     });
+  }, [user]);
+
+  const handleChooseImage = async () => {
+    try {
+      const image = await ImageCropPicker.openPicker({
+        width: 300,
+        height: 300,
+        cropping: true,
+      });
+
+      const uploadTask = firebase.storage().ref(`profileImages/${user.uid}`).putFile(image.path);
+      uploadTask.on('state_changed', 
+        (snapshot) => {
+          // Acompanhar o progresso do upload
+        },
+        (error) => {
+          console.error('Erro ao fazer upload da imagem:', error);
+        },
+        async () => {
+          const downloadURL = await uploadTask.snapshot.ref.getDownloadURL();
+          const userRef = firestore().collection('profileImages').doc(user.uid);
+
+          userRef.get().then((docSnapshot) => {
+            if (docSnapshot.exists) {
+              userRef.update({
+                profileImageUrl: downloadURL
+              }).then(() => {
+                setProfilePic(downloadURL);
+              }).catch((error) => {
+                console.error('Erro ao atualizar o documento do usuário:', error);
+              });
+            } else {
+              console.log('O documento do usuário não existe.');
+              userRef.set({
+                profileImageUrl: downloadURL
+              }).then(() => {
+                setProfilePic(downloadURL);
+              }).catch((error) => {
+                console.error('Erro ao criar o documento do usuário:', error);
+              });
+            }
+          }).catch((error) => {
+            console.error('Erro ao verificar a existência do documento do usuário:', error);
+          });
+        }
+      );
+    } catch (error) {
+      console.error('Ocorreu um erro ao selecionar a imagem:', error);
+    }
   };
 
-  async function uploadProfile() {
-    
-  }
-
-  const uploadFile = () => {
-    alert('text')
-  }
-
- return (
-   <View style={styles.container}>
-    <View style={styles.profileContainer}>
-      <TouchableOpacity onPress={handleChooseImage}>
-        <Image 
-          source={profilePic ? { uri: profilePic } : require('../../assets/default-profile-pic.jpg')}
-          style={styles.profilePic} 
-        />
-        <View style={styles.editIconContainer}>
-          <Feather name="edit-3" size={24} color="#FFF" />
-        </View>
-      </TouchableOpacity>
-      <Text style={styles.username}>{user ? user.nome : 'Nome do Usuário'}</Text>
+  return (
+    <View style={styles.container}>
+      <View style={styles.profileContainer}>
+        <TouchableOpacity onPress={handleChooseImage}>
+          <Image 
+            source={profilePic ? { uri: profilePic } : require('../../assets/default-profile-pic.jpg')}
+            style={styles.profilePic} 
+          />
+          <View style={styles.editIconContainer}>
+            <Feather name="edit-3" size={24} color="#FFF" />
+          </View>
+        </TouchableOpacity>
+        <Text style={styles.username}>{user ? user.nome : 'Nome do Usuário'}</Text>
+      </View>
+      <View style={styles.logoutContainer}>
+        <TouchableOpacity style={styles.logoutButton} onPress={() => signOut()}>
+          <Text style={styles.logoutText}>Sair</Text>
+        </TouchableOpacity>
+      </View>
     </View>
-    <View style={styles.stats}>
-      {/* Adicione as estatísticas do perfil, como número de posts, seguidores, seguindo, etc. */}
-    </View>
-    <TouchableOpacity style={styles.logoutButton} onPress={() => signOut()}>
-      <Text style={styles.logoutText}>Sair</Text>
-    </TouchableOpacity>
-   </View>
   );
 }
 
@@ -60,11 +101,6 @@ const styles = StyleSheet.create({
   container: {
     flex: 1,
     padding: 20,
-  },
-  menuOptionText: {
-    padding: 10,
-    fontSize: 16,
-    color: '#333',
   },
   profileContainer: {
     alignItems: 'center',
@@ -89,20 +125,19 @@ const styles = StyleSheet.create({
     fontSize: 24,
     fontWeight: 'bold',
   },
-  stats: {
-    flexDirection: 'row',
-    justifyContent: 'space-between',
-    marginBottom: 20,
+  logoutContainer: {
+    flex: 1,
+    alignItems: 'center',
   },
   logoutButton: {
+    width: '35%',
     backgroundColor: '#B22222',
-    borderRadius: 20,
+    borderRadius: 30,
     paddingVertical: 10,
-    paddingHorizontal: 10,
     alignItems: 'center',
   },
   logoutText: {
     color: '#FFF',
-    fontSize: 16,
+    fontSize: 18,
   },
 });
